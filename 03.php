412ce1a4cb5c625594588395308e290e27f9c6d6
Tatsuya <tm33keiogk@gmail.com>
2020-07-13 05:52:07 +0000

    re

--- 03.php
+++ 03.php
@@ -1,27 +1,27 @@
 <?php
 
 $name = "Tatsuya";
-if($name = "Tatsuya"){
-echo "私はTatsuyaです";
+if($name == "Naoya"){
+  echo "私はTatsuyaです";
 }else{
-echo "あなたの名前ではありません";
+  echo "あなたの名前ではありません";
 }
 
 $total = 0;
 for ($i = 0; $i <= 10000; $i++){
 $total += $i;
 }
-echo $total;
+  echo $total;
 
 $fruits = array("apple","banana","lemon","grape","orange");
 foreach($fruits as $fruit){
-echo $fruit;
+  echo $fruit;
 }
 
 $start = 1;
-$end = 101;
+$end = 100;
 
-for($i = $start; $i < $end; $i++){
+for($i = $start; $i <= $end; $i++){
 
   if($i % 5 == 0){
     echo $i;
